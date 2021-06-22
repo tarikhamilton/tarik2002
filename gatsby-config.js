e40a04ed58b14ff2a80644087b1ed33eda63e6db
Tarik Hamilton <me@tarikhamilton.com>
2021-06-22 09:48:40 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+  ],
+};
