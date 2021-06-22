0fb3801f8062684aaa442f1e7a72f98d69a149cd
Tarik Hamilton <me@tarikhamilton.com>
2021-06-22 13:50:13 -0400

    Update links and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        serif: '"Times New Roman", serif',
+      },
       colors: {
         yellow: '#FF0',
       },
